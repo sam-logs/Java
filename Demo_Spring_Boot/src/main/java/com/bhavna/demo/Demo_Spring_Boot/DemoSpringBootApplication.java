cb8c67ccf2396f1928be80d6105fe773ce0b2a02
Saumya.Prabhakar <Saumya.Prabhakar@BHAVNAWKS788.BHAVNACORP.NET>
2022-10-17 10:07:19 +0530

    Session 1 and 2 Task on 13-10-2022

---
+++ Demo_Spring_Boot/src/main/java/com/bhavna/demo/Demo_Spring_Boot/DemoSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.bhavna.demo.Demo_Spring_Boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootApplication.class, args);
+		System.out.println("Spring Boot");
+	}
+
+}
---
+++ Demo_Spring_Boot/src/test/java/com/bhavna/demo/Demo_Spring_Boot/DemoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhavna.demo.Demo_Spring_Boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
